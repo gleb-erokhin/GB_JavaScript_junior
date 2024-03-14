2a077dce02de40bde5e88fb8173ed8f86e3ce6f5
gleb-erokhin <gleb.erokhin@gmail.com>
2024-03-14 05:46:35 +0300

    задача 2 и 3

--- main.js
+++ main.js
@@ -2,13 +2,26 @@
 
 // 1. Найти по id, используя getElementById, элемент с id равным "super_link" и вывести этот элемент в консоль.
 
+// найдено по новому запросу
 // const link = document.querySelector('#super_link');
+
+// найдено по старому запросу
 const link = document.getElementById('super_link');
 console.log(link);
 
 // 2. Внутри всех элементов на странице, которые имеют класс "card-link", поменяйте текст внутри элемента на "ссылка".
 
+const linkElement = document.querySelectorAll('.card-link');
+
+// проходимся по массиву, и каждому елементу меняем текст ссылки
+linkElement.forEach(element => {
+    element.textContent = 'ссылка';
+});
+
 // 3. Найти все элементы на странице с классом "card-link", которые лежат в элементе с классом "card-body" и вывести полученную коллекцию в консоль.
+
+const showCardBoby = document.querySelectorAll('.card-body > .card-link');
+console.log(showCardBoby);
 
 // 4. Найти первый попавшийся элемент на странице у которого есть атрибут data-number со значением 50 и вывести его в консоль.
 
