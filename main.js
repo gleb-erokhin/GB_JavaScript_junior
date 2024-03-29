a899dc25aa7bcd131ffd8e3d2fcdf8860312d940
gleb-erokhin <gleb.erokhin@gmail.com>
2024-03-29 20:53:06 +0300

    асинхронная функция

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // обработка JSON с помощью ассинхронного запроса,  работает по принципу "обещание"
+const url = "./data.json";
+
+// начинаем работать с ассинхроном
+async function fetchData(url) {
+    // пробуем получить данные, если не получим, то ловим ошибку через catch
+    try {
+        // ожидаем ответ от сервера или можем получить 404
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`ошибка - ${error}`);
+    }
+}
+
+const data = await fetchData(url);
+console.log(data);
