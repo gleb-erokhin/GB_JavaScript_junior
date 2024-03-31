feafcf9bbdfff685d5e17806f11dad55cf6af915
gleb-erokhin <gleb.erokhin@gmail.com>
2024-03-31 22:06:31 +0300

    задание 1 ок

--- main.js
+++ main.js
@@ -16,15 +16,17 @@ let error = false;
 btn.addEventListener("click", (e) => {
     // для того чтобы при нажатии на кнопку ничего не отправлялось и страница не перегружалась (форма не отправляется)
     e.preventDefault();
-    // нуно проверять состояние ЧЕКБОСа, если он не нажат, обращаемся к INPUT к атрибуту checked, тогда
+    // нуно проверять состояние ЧЕКБОСа, если он не нажат, обращаемся к INPUT к атрибуту checked, и не ERROR тогда
     if (!input.checked && !error) {
         // создаем константу для текста условий
         const p = document.createElement("p");
         p.textContent = "Необходимо согласиться с условиями";
+        // добавим класс ошибки к сообщению (можно стилизовать по классу)
         p.classList.add('error');
         // сообщение отобразится ПОСЛЕ кнопки отправить
         btn.insertAdjacentElement("afterend", p);
         error = true;
+    // если галочка выбрана, то отправить форму через метод submit()
     } else if (input.checked) {
         form.submit();
     }
