19c3b5389a55ca30f4eb75f7d80017e886294b8b
gleb-erokhin <gleb.erokhin@gmail.com>
2024-04-01 05:41:22 +0300

    задание 2

--- main.js
+++ main.js
@@ -7,28 +7,36 @@
 // Если элемент не выбран, добавить текст под чекбоксом “Необходимо согласиться с условиями”
 
 // создаем переменные с нужными классами
-const input = document.querySelector('input');
-const form = document.querySelector('form');
-const btn = document.querySelector('button');
-let error = false;
+// const input = document.querySelector('input');
+// const form = document.querySelector('form');
+// const btn = document.querySelector('button');
+// let error = false;
 
 // добавлям обработчик события КЛИК для кнопки
-btn.addEventListener("click", (e) => {
-    // для того чтобы при нажатии на кнопку ничего не отправлялось и страница не перегружалась (форма не отправляется)
-    e.preventDefault();
-    // нуно проверять состояние ЧЕКБОСа, если он не нажат, обращаемся к INPUT к атрибуту checked, и не ERROR тогда
-    if (!input.checked && !error) {
-        // создаем константу для текста условий
-        const p = document.createElement("p");
-        p.textContent = "Необходимо согласиться с условиями";
-        // добавим класс ошибки к сообщению (можно стилизовать по классу)
-        p.classList.add('error');
-        // сообщение отобразится ПОСЛЕ кнопки отправить
-        btn.insertAdjacentElement("afterend", p);
-        // параметр true не дает возможность повторять ошибку согласия с условиями
-        error = true;
-    // если галочка выбрана, то отправить форму через метод submit()
-    } else if (input.checked) {
-        form.submit();
-    }
-})
+// btn.addEventListener("click", (e) => {
+//     // для того чтобы при нажатии на кнопку ничего не отправлялось и страница не перегружалась (форма не отправляется)
+//     e.preventDefault();
+//     // нуно проверять состояние ЧЕКБОСа, если он не нажат, обращаемся к INPUT к атрибуту checked, и не ERROR тогда
+//     if (!input.checked && !error) {
+//         // создаем константу для текста условий
+//         const p = document.createElement("p");
+//         p.textContent = "Необходимо согласиться с условиями";
+//         // добавим класс ошибки к сообщению (можно стилизовать по классу)
+//         p.classList.add('error');
+//         // сообщение отобразится ПОСЛЕ кнопки отправить
+//         btn.insertAdjacentElement("afterend", p);
+//         // параметр true не дает возможность повторять ошибку согласия с условиями
+//         error = true;
+//     // если галочка выбрана, то отправить форму через метод submit()
+//     } else if (input.checked) {
+//         form.submit();
+//     }
+// })
+
+
+// Задание2
+// В html создать 2 элемента радио кнопки (input type=”radio”) и текст “Чай”, “Кофе” соответственно
+// Кнопка отправить
+// Если выбран чай, необходимо вывести сообщение “Чай закончился”
+// Если выбран кофе, необходимо вывести соообщение “кофе закончился”
+
