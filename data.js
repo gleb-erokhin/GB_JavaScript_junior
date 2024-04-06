cc33131fdbfcec6acf6abfa8d307c93e92b385d2
gleb-erokhin <gleb.erokhin@gmail.com>
2024-04-06 17:46:21 +0300

    создание пробного файла json

---
+++ data.js
@@ -0,0 +1,23 @@
+const recipes = `
+[
+    {
+        "name": "Hydration-bread",
+        "servings": 8,
+        "calories": "272 kcal",
+        "bju": {
+            "protein": "69 g",
+            "fat": "23 g",
+            "carb": "412 g"
+            },
+        "contains": {
+            "Cholesterol": "0 mg",
+            "Sodium": "285 mg",
+            "Calcium": "12 mg",
+            "Magnesium": "18 mg",
+            "Potassium": "75 mg",
+            "Iron": "3 mg"
+        },
+        "img": "/img/Hydration-bread.jpg"
+    }
+]
+`
