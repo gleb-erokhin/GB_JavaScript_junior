b333e1904709d3d1663d6e2d4fc5d4f29843a5c9
gleb-erokhin <gleb.erokhin@gmail.com>
2024-04-06 19:00:02 +0300

    задание выполнено

--- data.js
+++ data.js
@@ -1,23 +1,61 @@
 const recipes = `
 [
     {
-        "name": "Hydration-bread",
+        "name": "Hydration bread",
         "servings": 8,
-        "calories": "272 kcal",
+        "calories": 272,
         "bju": {
-            "protein": "69 g",
-            "fat": "23 g",
-            "carb": "412 g"
+            "protein": 69,
+            "fat": 23,
+            "carb": 412
             },
         "contains": {
-            "Cholesterol": "0 mg",
-            "Sodium": "285 mg",
-            "Calcium": "12 mg",
-            "Magnesium": "18 mg",
-            "Potassium": "75 mg",
-            "Iron": "3 mg"
+            "Cholesterol": 0,
+            "Sodium": 285,
+            "Calcium": 12,
+            "Magnesium": 18,
+            "Potassium": 75,
+            "Iron": 3 
         },
         "img": "/img/Hydration-bread.jpg"
+    },
+    {
+        "name": "Eggy bread",
+        "servings": 1,
+        "calories": 395,
+        "bju": {
+            "protein": 17,
+            "fat": 22,
+            "carb": 28
+            },
+        "contains": {
+            "Cholesterol": 359,
+            "Sodium": 407,
+            "Calcium": 142,
+            "Magnesium": 36,
+            "Potassium": 226,
+            "Iron": 3
+        },
+        "img": "/img/Eggy-bread.jpg"
+    },
+    {
+        "name": "Milk bread",
+        "servings": 15,
+        "calories": 12,
+        "bju": {
+            "protein": 8,
+            "fat": 2,
+            "carb": 30
+            },
+        "contains": {
+            "Cholesterol": 0,
+            "Sodium": 3,
+            "Calcium": 10,
+            "Magnesium": 1,
+            "Potassium": 13,
+            "Iron": 0
+        },
+        "img": "/img/milk-bread.jpg"
     }
 ]
 `
--- main.js
+++ main.js
@@ -2,7 +2,61 @@ const productData = JSON.parse(recipes);
 
 const wraper = document.querySelector('.wraper');
 productData.forEach(element => {
-    wraper.insertAdjacentHTML('beforeend', `
-    
+    wraper.insertAdjacentHTML('beforeend', 
+    `
+        <div class="wraper__container">
+            <h1 class="wraper__title">${element.name}</h1>
+            <img src="${element.img}" alt="" class="wraper__photo">
+            <div class="wraper-contains">
+                <div class="wraper-contains__item wraper-contains__item_info">
+                    <p>${element.servings} порций</p>
+                    <p>${element.calories} ккал</p>
+                </div>
+                <div class="wraper-contains__item">
+                    <ul class="wraper-contains__lists">
+                        <li class="wraper-contains__list">
+                            <span>Белки</span>
+                            <span>${element.bju.protein} gr</span>
+                        </li>
+                        <li class="wraper-contains__list">
+                            <span>Жиры</span>
+                            <span>${element.bju.fat} gr</span>
+                        </li>
+                        <li class="wraper-contains__list">
+                            <span>Углеводы</span>
+                            <span>${element.bju.carb} gr</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="wraper-contains__item">
+                    <ul class="wraper-contains__lists">
+                        <li class="wraper-contains__list">
+                            <span>Холестерин</span>
+                            <span>${element.contains.Cholesterol} gr</span>
+                        </li>
+                        <li class="wraper-contains__list">
+                            <span>Натрий</span>
+                            <span>${element.contains.Sodium} gr</span>
+                        </li>
+                        <li class="wraper-contains__list">
+                            <span>Кальций</span>
+                            <span>${element.contains.Calcium} gr</span>
+                        </li>
+                        <li class="wraper-contains__list">
+                            <span>Магний</span>
+                            <span>${element.contains.Magnesium} gr</span>
+                        </li>
+                        <li class="wraper-contains__list">
+                            <span>Калий</span>
+                            <span>${element.contains.Potassium} gr</span>
+                        </li>
+                        <li class="wraper-contains__list">
+                            <span>Железо</span>
+                            <span>${element.contains.Iron} gr</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     `)
 });
